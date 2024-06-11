162f09128823671832135f4e02321268a2d83e5d
Yoana Naydenova <y.naydenova@gmail.com>
2024-06-11 20:28:00 +0300

    Create Home page

---
+++ src/pages/Home.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div className="container">
+      <div className="py-5">
+        <table className="table border shadow caption-top table-hover">
+          <caption>List of items</caption>
+          <thead>
+            <tr>
+              <th scope="col">#</th>
+              <th scope="col">First</th>
+              <th scope="col">Last</th>
+              <th scope="col">Handle</th>
+            </tr>
+          </thead>
+          <tbody className="table-group-divider">
+            <tr>
+              <th scope="row">1</th>
+              <td>Mark</td>
+              <td>Otto</td>
+              <td>@mdo</td>
+            </tr>
+            <tr>
+              <th scope="row">2</th>
+              <td>Jacob</td>
+              <td>Thornton</td>
+              <td>@fat</td>
+            </tr>
+            <tr>
+              <th scope="row">3</th>
+              <td colspan="2">Larry the Bird</td>
+              <td>@twitter</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
