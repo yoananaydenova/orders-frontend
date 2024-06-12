147ddc5506f4f21530db205ba2c5dd258f819f51
Yoana Naydenova <y.naydenova@gmail.com>
2024-06-12 16:26:42 +0300

    Add Home page to Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,13 +3,16 @@ import React from "react";
 const Navbar = () => {
   return (
     <>
-      <nav class="navbar navbar-expand-lg navbar-dark bg-secondary pb-0 px-5">
+      <nav className="navbar navbar-expand-lg bg-secondary pb-0 px-5">
         <h5 className="text-light px-5">Fullstack Orders App</h5>
         <div className="d-inline-flex gap-3">
-          <button type="button" className="navbtn">
+          <button type="button" className="btn-nav btn-home">
+            Home
+          </button>
+          <button type="button" className="btn-nav">
             Items
           </button>
-          <button type="button" className="navbtn">
+          <button type="button" className="btn-nav">
             Orders
           </button>
         </div>
