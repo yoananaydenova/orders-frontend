c97aacf77dce5b482352601a4fb8180a40371d79
Yoana Naydenova <y.naydenova@gmail.com>
2024-06-12 16:26:14 +0300

    Add Home page image and card

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,39 +1,26 @@
 import React from "react";
+import image from "../assets/bg-desk.jpg";
 
 const Home = () => {
   return (
-    <div className="container">
-      <div className="py-5">
-        <table className="table border shadow caption-top table-hover">
-          <caption>List of items</caption>
-          <thead>
-            <tr>
-              <th scope="col">#</th>
-              <th scope="col">First</th>
-              <th scope="col">Last</th>
-              <th scope="col">Handle</th>
-            </tr>
-          </thead>
-          <tbody className="table-group-divider">
-            <tr>
-              <th scope="row">1</th>
-              <td>Mark</td>
-              <td>Otto</td>
-              <td>@mdo</td>
-            </tr>
-            <tr>
-              <th scope="row">2</th>
-              <td>Jacob</td>
-              <td>Thornton</td>
-              <td>@fat</td>
-            </tr>
-            <tr>
-              <th scope="row">3</th>
-              <td colspan="2">Larry the Bird</td>
-              <td>@twitter</td>
-            </tr>
-          </tbody>
-        </table>
+    <div className="position-relative mh-100">
+      <img
+        src={image}
+        className="img-fluid object-fit-cover"
+        alt="Home page with instructions"
+      ></img>
+
+      <div className="card position-absolute top-0 start-50 translate-middle-x mt-5">
+        <div className="card-body">
+          <h5 className="card-title">Card title</h5>
+          <p className="card-text">
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </p>
+          <a href="#" className="btn btn-primary">
+            Go somewhere
+          </a>
+        </div>
       </div>
     </div>
   );
