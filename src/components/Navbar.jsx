4747344f6378509731631e3c2c6a2ec5577818df
Yoana Naydenova <y.naydenova@gmail.com>
2024-06-12 17:24:38 +0300

    Change navbar to works with routing

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,20 +1,23 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 
 const Navbar = () => {
   return (
     <>
       <nav className="navbar navbar-expand-lg bg-secondary pb-0 px-5">
-        <h5 className="text-light px-5">Fullstack Orders App</h5>
+        <NavLink to="/" className="fs-4 fw-bold text-light px-5">
+          Fullstack Orders App
+        </NavLink>
         <div className="d-inline-flex gap-3">
-          <button type="button" className="btn-nav btn-home">
+          <NavLink to="/" className="btn-nav btn-home">
             Home
-          </button>
-          <button type="button" className="btn-nav">
+          </NavLink>
+          <NavLink to="/items" className="btn-nav">
             Items
-          </button>
-          <button type="button" className="btn-nav">
+          </NavLink>
+          <NavLink to="/orders" className="btn-nav">
             Orders
-          </button>
+          </NavLink>
         </div>
       </nav>
     </>
