0ed48c0d45bd7ceef98f6db5c14d94733160d3db
Yoana Naydenova <y.naydenova@gmail.com>
2024-06-16 22:48:52 +0300

    Configure Routing using REACT-ROUTER-DOM

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,28 @@
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar.jsx";
 import Home from "./pages/Home.jsx";
+import Items from "./pages/Items.jsx";
+import Orders from "./pages/Orders.jsx";
+import AddItem from "./components/items/AddItem";
+import EditItem from "./components/items/EditItem";
+import AddOrder from "./components/orders/AddOrder.jsx";
 
 function App() {
   return (
-    <div >
+    <BrowserRouter>
       <Navbar />
-      <Home />
-    </div>
+      <Routes>
+        <Route element={<Home />} path="/" />
+        <Route element={<Items />} path="/items" />
+        <Route element={<AddItem />} path="/add-item" />
+        <Route element={<EditItem />} path="/edit-item" />
+
+        <Route element={<Orders />} path="/orders" />
+        <Route element={<AddOrder />} path="/add-order" />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
