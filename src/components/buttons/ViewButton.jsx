4075c2c4298f7f348bdaef1c2908f67996cf2337
Yoana Naydenova <y.naydenova@gmail.com>
2024-06-20 23:05:53 +0300

    Fix button property names

--- src/components/buttons/ViewButton.jsx
+++ src/components/buttons/ViewButton.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-const ViewButton = ({to}) => {
+const ViewButton = ({ to }) => {
   return (
     <Link to={to} className="btn btn-light border border-dark-subtle mx-2">
       <svg
@@ -9,7 +9,7 @@ const ViewButton = ({to}) => {
         width="16"
         height="16"
         fill="currentColor"
-        class="bi bi-search"
+        className="bi bi-search"
         viewBox="0 0 16 16"
       >
         <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001q.044.06.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1 1 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0" />
