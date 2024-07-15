ddd6aceec3685a321687d2f60b54c89070432bbd
Yoana Naydenova <y.naydenova@gmail.com>
2024-07-15 21:38:31 +0300

    Fix SimpleButton property names

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,12 @@ import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 import Items from "./pages/Items";
 import Orders from "./pages/Orders";
-import AddItem from "./components/items/AddItem";
+import CreateItem from "./components/items/CreateItem";
 import EditItem from "./components/items/EditItem";
 import ViewItem from "./components/items/ViewItem";
-import AddOrder from "./components/orders/AddOrder";
-import EditOrder from './components/orders/EditOrder';
-import ViewOrder from './components/orders/ViewOrder';
+import CreateOrder from "./components/orders/CreateOrder";
+import EditOrder from "./components/orders/EditOrder";
+import ViewOrder from "./components/orders/ViewOrder";
 
 function App() {
   return (
@@ -20,14 +20,14 @@ function App() {
         <Route element={<Home />} path="/" />
 
         <Route element={<Items />} path="/items" />
-        <Route element={<AddItem />} path="/add-item" />
+        <Route element={<CreateItem />} path="/add-item" />
         <Route element={<EditItem />} path="/edit-item/:id" />
         <Route element={<ViewItem />} path="/view-item/:id" />
 
         <Route element={<Orders />} path="/orders" />
-        <Route element={<AddOrder />} path="/add-order" />
-        <Route element={<EditOrder />} path="/edit-order/:id"/>
-        <Route element={<ViewOrder />} path="/view-order/:id"/>
+        <Route element={<CreateOrder />} path="/add-order" />
+        <Route element={<EditOrder />} path="/edit-order/:id" />
+        <Route element={<ViewOrder />} path="/view-order/:id" />
       </Routes>
     </BrowserRouter>
   );
