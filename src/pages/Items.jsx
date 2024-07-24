70d7e89cc47988105c401c7902c979b8b1866677
Yoana Naydenova <y.naydenova@gmail.com>
2024-07-24 15:52:14 +0300

    Replace table with ItemTable component

--- src/pages/Items.jsx
+++ src/pages/Items.jsx
@@ -8,6 +8,7 @@ import {
   ViewButton,
 } from "../components/buttons/LinkButton";
 import toast from "react-hot-toast";
+import ItemTable from "../components/common/ItemTable";
 
 const Items = () => {
   const [items, setItems] = useState([]);
@@ -29,40 +30,20 @@ const Items = () => {
     toast.success("The item is successfully deleted!");
   };
 
-  // TODO replace with ItemTable Component
   return (
     <div className="container">
       <AddButton name="Add item" to="/add-item" />
       <div className="py-3">
-        <table className="table border shadow caption-top table-hover">
-          <caption>List of items</caption>
-          <thead>
-            <tr>
-              <th scope="col">ID</th>
-              <th scope="col">ITEM NAME</th>
-              <th scope="col">QUANTITY</th>
-              <th scope="col">PRICE</th>
-              <th scope="col" className="col-md-3 offset-md-3">
-                ACTION
-              </th>
-            </tr>
-          </thead>
-          <tbody className="table-group-divider align-middle">
-            {items.map((item, index) => (
-              <tr key={item.id}>
-                <th scope="row">{item.id}</th>
-                <td>{item.name}</td>
-                <td>{item.quantity}</td>
-                <td>{item.price}</td>
-                <td className="btn-group-sm">
-                  <ViewButton to={`/view-item/${item.id}`} />
-                  <EditButton to={`/edit-item/${item.id}`} />
-                  <DeleteButton onClick={() => deleteItem(item.id)} />
-                </td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
+        <ItemTable
+          items={items}
+          buttons={(item) => (
+            <>
+              <ViewButton to={`/view-item/${item.id}`} />
+              <EditButton to={`/edit-item/${item.id}`} />
+              <DeleteButton onClick={() => deleteItem(item.id)} />
+            </>
+          )}
+        />
       </div>
     </div>
   );
